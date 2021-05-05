39f34ee24274c8b41e92ff9088e86995ea00f387
tn64 <77516099+tn64@users.noreply.github.com>
2021-05-05 16:27:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
 #Create dependencies
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-#Create a New Flask App Instance
+#Set Up the Database
+engine = create_engine('sqlite:///hawaii.sqlite')
+Base = automap_base()
+
+#Reflect Tables in Database
+Base.prepare(engine, reflect=True)
+
+#Save References to Each Table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Create a Session Link from Python to the Database
+session = Session(engine)
+
+#Set Up Flask - Create a New Flask App Instance
 app = Flask(__name__)
 
-#Create Flask Routes
+#All Routes Should Go AFTER the app = FLASK(__name__)
+#Create Flask Routes. The first one is the 'welcome' route
 @app.route('/')
-def hello_world():
-    return 'Hello world'
+
+#Create welcome() function. When creating routes, follow the 
+# naming convention /api/v1.0/ followedc by the name of the routh.
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+#Create precipitation routh
+@app.route('/api/v1.0/precipitation')
+
+#Create precipitation function
+def precipitation():
+    prev_year = dt.datetime(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
